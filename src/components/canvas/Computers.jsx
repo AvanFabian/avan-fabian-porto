232eb25a4f0a5ccebae8bef7143d4388f303f4fb
AvanFabian <avanfabiandaniswara@gmail.com>
2023-08-01 11:35:14 +0700

    Change-Computer 3D model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,13 +1,14 @@
 import { Suspense, useEffect, useState } from 
 "react";
-import { Canvas } from 
+import { Canvas, useFrame } from 
 "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from 
 "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const Computer = useGLTF('./comp/scene.gltf')
+  const Computer = useGLTF('./cyberpunkdesk/scene.gltf')
+
   return (
     // mesh tag purpose is to add light to the scene
     <mesh>
@@ -24,9 +25,9 @@ const Computers = ({ isMobile }) => {
       />
       <primitive
         object={Computer.scene}
-        scale={isMobile ? 0.5 : 0.75}
-        position={isMobile ? [-1.5, -3.25, -1.5] : [0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
+        scale={isMobile ? 9 : 11}
+        position={isMobile ? [-1.5, -3.25, -1.5] : [22, -40.25, -2.5]}
+        rotation={[-0.01, 1, 0,2]}
       />
       </mesh>
   )
@@ -65,8 +66,9 @@ const ComputersCanvas = () => {
       shadows
       // position: [20, 3, 5] is for camera position
       // fov: 50 is for camera zoom
-      camera={{ position: [20, 3, 5], fov: 25 }}
+      camera={{ position: [25, 12, 24], fov: 40 }}
       gl = {{ preserveDrawingBuffer: true }}
+      
     >
     { /* don't render or call any file if the content is still including an html tag like div etc. */ }
     { /* <Suspense fallback={<canvasLoader />}> */}
@@ -74,7 +76,7 @@ const ComputersCanvas = () => {
       <Suspense fallback={<CanvasLoader />}>
       {/* Suspense is for loading the 3d model */}
         <OrbitControls 
-          enableZoom={false} 
+          enableZoom={false}
           // maxPolarAngle is for the camera to not rotate vertically
           maxPolarAngle={Math.PI / 2}
           // minPolarAngle is for the camera to not rotate vertically
